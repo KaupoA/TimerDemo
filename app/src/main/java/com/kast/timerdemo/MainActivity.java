5891053065efbc27e919aa4ac3897460744084c7
Kaupo Aun <kaupo.aun@gmail.com>
2017-11-16 23:11:49 +0200

    Finished demo

--- app/src/main/java/com/kast/timerdemo/MainActivity.java
+++ app/src/main/java/com/kast/timerdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.kast.timerdemo;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new CountDownTimer(10000, 1000) {
+
+            public void onTick(long millisecondsUntilDone) {
+
+                // Counter is counting down (every second)
+
+                Log.i("Seconds left", String.valueOf(millisecondsUntilDone / 1000));
+
+            }
+
+            public void onFinish() {
+
+                // Counter is finished (after 10 seconds
+
+                Log.i("Done!", "Countdown timer finished!");
+
+            }
+
+        }.start();
+
+        /*
+        final Handler handler = new Handler();
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+
+                // Insert code to be run every second
+
+                Log.i("Runnable must run! ", "A second must have passed...");
+
+                handler.postDelayed(this, 1000);
+
+            }
+        };
+
+        handler.post(run);
+
+        */
     }
+
 }
